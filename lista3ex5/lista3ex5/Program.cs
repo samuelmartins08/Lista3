6ff39412b0ec8d2a7ed040a4bdd9c6cf6d3b1e65
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-06-07 20:42:39 -0300

    Add files via upload

---
+++ lista3ex5/lista3ex5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lista3ex5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num, r;
+
+            Console.WriteLine("Numero da tabuada a ser feita: ");
+            num = Convert.ToDouble(Console.ReadLine());
+            if (num < 0)
+
+                do
+                {
+                    Console.WriteLine("Você digitou um digito inválido, por favor digite um número positivo valido: ");
+                    num = Convert.ToDouble(Console.ReadLine());
+                } while (num < 0);
+
+            for (int i = 1; i < 11; i++)
+            {
+
+
+                r = num * i;
+                Console.WriteLine("{0} X {1}= {2} ", num, i, r);
+
+            }
+        }
+    }
+}
