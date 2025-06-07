71ae71ca3adb4ef5dc3e68ba5fc092bcc145ed4c
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-06-07 20:52:54 -0300

    Add files via upload

---
+++ lista3ex7/lista3ex7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Ex7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string p;
+
+            double a = 1, b = 1, c;
+
+            for (int i = 1; i < 21; i++)
+            {
+                Console.Write("Digite algo");
+                p = Convert.ToString(Console.ReadLine());
+                do
+                {
+                    c = a * b;
+                    Console.WriteLine("{0}x{1}={2}", a, b, c);
+                    b++;
+                } while (b < 11);
+                a++;
+                b = 1;
+            }
+        }
+    }
+}
