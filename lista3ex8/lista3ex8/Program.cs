07d45d38199e8a1d3a21bb738347a51a958d7b0c
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-06-07 20:56:10 -0300

    Add files via upload

---
+++ lista3ex8/lista3ex8/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Ex8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double soma = 0;
+           
+            for (int i = 1; i <= 100; i++)
+            {
+
+                soma = soma + i;
+
+
+                Console.WriteLine("{0} intervalo:{1}", soma, i);
+
+            }
+
+        }
+    }
+}
