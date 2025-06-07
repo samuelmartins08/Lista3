92d7aeccca43875ad0fdd3fdddfc9e28ff95442f
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-06-07 20:45:20 -0300

    Add files via upload

---
+++ lista3ex6/lista3ex6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Ex6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double va, vb, m, t;
+            do
+            {
+                Console.Write("Qual valor deseja fazer a tabuada: ");
+                m = Convert.ToDouble(Console.ReadLine());
+                Console.Write("A tabuada deve ser feita a partir de qual número? ");
+                va = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Até o número? ");
+                vb = Convert.ToDouble(Console.ReadLine());
+                t = m * va;
+            }
+            while (va < 0 || vb < 0 || t <= 0);
+
+
+            if (vb <= va)
+
+            {
+                Console.WriteLine("Digite o valor de B: ");
+
+                vb = Convert.ToDouble(Console.ReadLine());
+            }
+
+            do
+            {
+                t = m * va;
+                Console.WriteLine("{0} X {1} = {2}", m, va++, t);
+
+            }
+
+            while (va <= vb);
+        }
+    }
+}
+
