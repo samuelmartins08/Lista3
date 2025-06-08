48f684822d2b5e1afc6005bc88bc8d01c73e48f7
samuelmartins08 <samuel.martins@aluno.ifsp.edu.br>
2025-06-07 21:00:01 -0300

    Add files via upload

---
+++ lista3ex9/lista3ex9/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista3Ex9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("SEQUENCIA DE FIBONNACI DOS 30 PRIMEIROS NÚMEROS: ");
+            int a = 0, b = 1, c;
+
+            Console.WriteLine("{0} ", b);
+
+            for (int i = 0; i < 29; i++)
+
+            {
+
+                c = a + b;
+                Console.WriteLine("{0} ", c);
+                a = b;
+                b = c;
+
+            }
+        }
+    }
+}
